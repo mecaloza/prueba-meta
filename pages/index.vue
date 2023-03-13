ef4d58db942620563a27389a45c4aa650e47d81f
DEV Esteban L <lozadagiraldo.esteban111@gmail.com>
2023-03-13 16:36:45 -0500

    primer commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <Tutorial />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Prueba Esteban",
+    };
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Este es la description",
+        },
+      ],
+    };
+  },
+};
+</script>
